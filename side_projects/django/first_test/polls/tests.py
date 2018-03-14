3886e7271c07ba2403b7d3e7882dbf544c45f3dd
Otomakan <jgmisteli@gmail.com>
2018-03-14 16:59:15 -0500

    Last tests

--- side_projects/django/first_test/polls/tests.py
+++ side_projects/django/first_test/polls/tests.py
@@ -24,7 +24,7 @@ def create_question(question_text, days):
 	time = timezone.now() + datetime.timedelta(days=days)
 	return Question.objects.create(question_text=question_text, pub_date=time)
 
-class QuestionIndexVIewTests(TestCase):
+class QuestionIndexViewTests(TestCase):
 	def test_no_question(self):
 		response = self.client.get(reverse('polls:index'))
 		self.assertEqual(response.status_code, 200)
@@ -36,9 +36,28 @@ class QuestionIndexVIewTests(TestCase):
 		self.assertQuerysetEqual(response.context['latest_q_list'],['<Question: What;s Your favorite name?>']
 		)
 	def test_future_question(self):
-		create_question("Future Question", 10):
+		create_question("Future Question", 10)
 		res = self.client.get(reverse("polls:index"))
-		self.assertEqual(response.status_code, 200)
+		self.assertEqual(res.status_code, 200)
 		self.assertContains(res, " No polls here")
 		self.assertQuerysetEqual(res.context['latest_q_list'],[])
+	def test_future_and_two_past_questions(self):
+		create_question("Future Question", 10)
+		create_question("Past Question", -10)
+		create_question("More Qu", -12)
+		response = self.client.get(reverse('polls:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertQuerysetEqual(response.context['latest_q_list'],['<Question: Past Question>','<Question: More Qu>'])
+
+
+class QuestionDetailViewTests(TestCase):
+	def test_future_question(self):
+		future_q = create_question("Future Question",10)
 	
+		url =reverse('polls:detail', args=(future_q.id,))
+		res = self.client.get(url)
+		self.assertEqual(res.status_code, 404)
+	def test_past_question(self):
+		past_q = create_question("Past Question", -10)
+		res = self.client.get(reverse("polls:detail", args=(past_q.id,)))
+		self.assertContains(res, past_q.question_text)
